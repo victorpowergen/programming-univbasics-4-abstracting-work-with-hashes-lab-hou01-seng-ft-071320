a8b14779333819392791d5288e25283888140dd8
Adewale Olusegun Ige <victorpowergen@gmail.com>
2020-06-24 06:34:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,10 +3,8 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  { hash => key
-  if hash == key
-    puts hash
-  }
+   hash => key
+  
 end
 
 def update_counting_hash(hash, key)
